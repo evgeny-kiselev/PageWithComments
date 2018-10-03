189d70cbe54db189de4daab92e46a51485716807
zheny <kiselev.evgeny.n@gmail.com>
2018-10-03 20:17:27 +0500

    Created reg form

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name = "register")
+     */
+    public function register(Request $request)
+    {
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class, ['attr'=>['placeholder'=>'Введите логин']])
+            ->add('password', RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'first_options' => ['attr'=>['placeholder'=>'Введите пароль'], 'label'=>' '],
+                    'second_options' => ['attr'=>['placeholder'=>'Повторите пароль'], 'label'=>' ']
+                ])
+            ->add('save', SubmitType::class, ['label' => 'зарегистрироваться'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            return new Response("Регистрация успешна!");
+        }
+
+        return $this->render('register.html.twig', ['form'=>$form->createView()]);
+    }
+}
