087daef5ee67dad5b3608b17fa12734151081697
zheny <kiselev.evgeny.n@gmail.com>
2018-10-03 19:58:11 +0500

    Create login through database

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login");
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        return new Response('Hello');
+    }
+}
