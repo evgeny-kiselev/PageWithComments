df22cc8143a4017e5e5dd08b8a566c43968b9a7e
zheny <kiselev.evgeny.n@gmail.com>
2018-10-04 12:57:19 +0500

    Created comment form

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Page;
 use App\Entity\User;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,14 +17,16 @@ class PageController extends Controller
     public function page($page_id){
         $doc = $this->getDoctrine()->getRepository(Page::class);
         $page = $doc->find($page_id);
-        if($page != null) return $this->render('page.html.twig',['page'=>$page]);
-        else return new Response("NOT FOUND!", 404);
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        if($page != null)
+            return $this->render('page.html.twig',['page'=>$page,'user'=>$user,'user_role'=>$user]);
+        else return new Response("PAGE NOT FOUND!", 404);
     }
 
     /**
      * @Route("/", name = "p_load")
      */
     public function page_load(){
-        $this->page(1);
+        return $this->page(1);
     }
 }
