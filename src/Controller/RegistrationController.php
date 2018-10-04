ee901f7e0c175ffd45db0f440e8f7713d76702a0
zheny <kiselev.evgeny.n@gmail.com>
2018-10-04 19:11:06 +0500

    Added encode pass

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -18,17 +19,20 @@ class RegistrationController extends Controller
     /**
      * @Route("/register", name = "register")
      */
-    public function register(Request $request)
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $plainPass = $user->getPassword();
+            $encoded_pass = $encoder->encodePassword($user, $plainPass);
+            $user->setPassword($encoded_pass);
             $doc = $this->getDoctrine()->getManager();
             $doc->persist($user);
             $doc->flush();
-            return new Response("Регистрация успешна!");
+            return  $this->redirect("/login");
         }
 
         return $this->render('register.html.twig', ['form'=>$form->createView()]);
